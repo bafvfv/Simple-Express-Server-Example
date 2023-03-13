12189395c77f34759d032d3c757d1b71e152af93
AÇEH <75881051+febariev@users.noreply.github.com>
2023-03-13 20:15:24 +0500

    Update brandController.js

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,17 +1,21 @@
-const {Brand} = require('../models/models');
+const BrandService = require('../services/brandService');
 const ApiError = require('../error/ApiError');
 
 class BrandController{
 
-    async create(req, res) {
-        const {name} = req.body // указываем, что name будет в теле запроса 
-        const brand = await Brand.create({name}); // добавляем name в таблицу брэндов
-        return res.json(brand) // отправляем новый товар в результате, в оригинале будем отправлять сообщение об успешном
-    }                          // создании брэнда
+    async create(req, res, next) {
+        try {
+            const brand = await BrandService.createBrand(req.body)
+            return res.json(brand)
+
+        } catch(e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
 
     async getAll(req, res) {
-        const brand = await Brand.findAll();
-        return res.json(brand)
+        const all = await BrandService.getAllBrands()
+        return res.json(all)
     }
 
 }
