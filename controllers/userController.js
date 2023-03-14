f7281c2281b242aabcc13bd7d20d443a38b6087a
AÇEH <75881051+febariev@users.noreply.github.com>
2023-03-14 09:21:20 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
-const {User, Basket} = require('../models/models');
+const UserService = require('../services/userService');
 const ApiError = require('../error/ApiError');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -13,40 +12,32 @@ const generateJwt = (id, email, role) => {
 }
 
 class UserController{
+
     async registration(req, res, next) {
-        const {email, password, role} = req.body
-        if(!email || !password) {
-            return next(ApiError.badRequest('Поля емэйл и пароль обязательны к заполнению'))
+        try {
+            const user = await UserService.registerUser(req.body)
+            const token = generateJwt(user.id, user.email, user.role)
+            return res.json(token)
+        } catch(e) {
+            next(ApiError.badRequest(e.message))
         }
-        const isUser = await User.findOne({where:{email}});
-        if(isUser) {
-            return next(ApiError.badRequest('Пользователь с таким емэйлом уже зарегистрирован на сайте'))
-        }
-        const hashPassword = await bcrypt.hash(password, 5);
-        const user = await User.create({email, role, password: hashPassword});
-        const basket = await Basket.create({userId: user.id});
-        const token = generateJwt(user.id, user.email, user.role)
-        return res.json(token)
     }
 
     async login(req, res, next) {
-        const {email, password} = req.body
-        const user = await User.findOne({where:{email}});
-        if(!user) {
-            return next(ApiError.badRequest('Вы не зарегистрированы в системе'))
+        try {
+            const user = await UserService.userLogin(req.body)
+            const token = generateJwt(user.id, user.email, user.role)
+            return res.json(token)
+        } catch(e) {
+            next(ApiError.badRequest(e.message))
         }
-        let comparePassword = bcrypt.compareSync(password, user.password)
-        if(!comparePassword) {
-            return next(ApiError.internal('Указан неверный пароль'))
-        }
-        const token = generateJwt(user.id, user.email, user.role)
-        return res.json(token)
     }
 
     async check(req, res, next) {
         const token = generateJwt(req.user.id, req.user.email, req.user.role)
         return res.json({token})
     }
+
 }
 
 module.exports = new UserController()
